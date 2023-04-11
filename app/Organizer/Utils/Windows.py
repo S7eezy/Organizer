7584b167974addeeb9db788a7846cf57dd408649
S7eezy <cvrc.valentin@gmail.com>
2023-04-11 23:09:59 +0200

    Remove leftover verbose

--- app/Organizer/Utils/Windows.py
+++ app/Organizer/Utils/Windows.py
@@ -92,7 +92,6 @@ def switchToWindow(processName):
         data = enum_process_windows(pid)
         if data:
             for handle, text in data:
-                print(processName, text)
                 if processName == text.split(" -")[0]:
                     remote_thread, _ = wproc.GetWindowThreadProcessId(handle)
                     wproc.AttachThreadInput(wapi.GetCurrentThreadId(), remote_thread, True)
